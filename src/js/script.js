dd02a827c9b948cc313eaede19ec63db2984e2ee
V0L40K_35 <vol4ok94@gmail.com>
2020-05-05 02:45:16 +0600

    forms validate complete

--- src/js/script.js
+++ src/js/script.js
@@ -51,4 +51,37 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         });
     });
+
+    // form validate
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста введите своё имя",
+                    minlength: jQuery.validator.format("Введите минимум {0} символа")
+                },
+                phone: "Пожалуйста введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
   });
