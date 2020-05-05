c8d8ff541a3705d94d48b42ccda432478f76eda0
V0L40K_35 <vol4ok94@gmail.com>
2020-05-05 13:31:50 +0600

    add mask to input

--- src/js/script.js
+++ src/js/script.js
@@ -84,4 +84,6 @@ $(document).ready(function(){
     validateForms('#consultation form');
     validateForms('#order form');
 
+    // input mask
+    $('input[name=phone]').mask("+7 (999) 999-99-99")
   });
