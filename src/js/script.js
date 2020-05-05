aac7937abc87e7eae559f12844cf8b5ce0ad2e2d
V0L40K_35 <vol4ok94@gmail.com>
2020-05-05 15:01:23 +0600

    add mailer php

--- src/js/script.js
+++ src/js/script.js
@@ -85,5 +85,21 @@ $(document).ready(function(){
     validateForms('#order form');
 
     // input mask
-    $('input[name=phone]').mask("+7 (999) 999-99-99")
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // mailer
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "../mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut('slow');
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
   });
